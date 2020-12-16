ee126589d21687ce331db60b8d65f6f46f100496
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-16 15:48:48 +0100

    Very basic structure

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+var pulsanteLancia = document.getElementById("lancia");
+pulsanteLancia.addEventListener('click',
+function() {
+
+  var tuoTiro = Math.floor(Math.random()*6) + 1;
+  console.log(tuoTiro);
+  var npcTiro = Math.floor(Math.random()*6) + 1;
+  console.log(npcTiro);
+
+
+
+}
+);
